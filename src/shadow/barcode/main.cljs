4ee04f5b98adcde56a285d1399fde10a4d7029ae
Martin Kavalar <martin@nextjournal.com>
2022-07-27 23:39:35 +0200

    Try dyanmic import

--- src/shadow/barcode/main.cljs
+++ src/shadow/barcode/main.cljs
@@ -1,6 +1,11 @@
 (ns shadow.barcode.main
-  (:require ["/barcode-detector-zbar/BarcodeDetectorPolyfill.min.js$default" :as barcode-detector-polyfill]))
-
+  (:require [shadow.esm :refer (dynamic-import)]))
+#_#_
 (js/console.log "Native api support" (barcode-detector-polyfill/checkBarcodeDetectorSupport))
 (barcode-detector-polyfill/setupPolyfill)
+
+
+(-> (dynamic-import "/barcode-detector-zbar/BarcodeDetectorPolyfill.min.js")
+    (.then (fn [mod]
+             (js/console.log :mod mod))))
 
